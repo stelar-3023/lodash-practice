1b6be0b154a671ed4062a5b65936269a7a849af6
Steve Larsen <slarsen-3@att.net>
2021-05-25 18:50:06 -0500

    _.concat

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 var _ = require("lodash");
+
+let array = [
+  { name: "Steve", age: 45 },
+  { name: "Dawn", age: 51 },
+];
+let other = _.concat(array, { name: "Anthony", age: 31 });
+
+console.log(other);
